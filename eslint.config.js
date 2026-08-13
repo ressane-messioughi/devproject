3018d13f0eff1ce880e36ddfbbc86c55f1dafb26
Ressane Messioughi <219838131+ressane-messioughi@users.noreply.github.com>
2026-08-13 16:08:16 +0200

    chore: configuration d'ESLint et de Prettier

---
+++ eslint.config.js
@@ -0,0 +1,51 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
+export default [
+  { ignores: ['dist', 'dist-ssr'] },
+
+  js.configs.recommended,
+
+  {
+    files: ['**/*.{js,jsx}'],
+
+    languageOptions: {
+      ecmaVersion: 'latest',
+
+      sourceType: 'module',
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+
+      globals: {
+        ...globals.browser,
+      },
+    },
+
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+
+    rules: {
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+
+      'react/react-in-jsx-scope': 'off',
+      'react-refresh/only-export-components': 'warn',
+    },
+  },
+];
